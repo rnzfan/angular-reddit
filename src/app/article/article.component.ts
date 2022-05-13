aa7f0cfd279637432e6b746c6d8dc03e18213cd1
Roy Fan <rnzfan@gmail.com>
2022-05-13 22:19:46 +1200

    Added my Angular project

---
+++ src/app/article/article.component.ts
@@ -0,0 +1,32 @@
+import { Component, HostBinding, OnInit, Input } from '@angular/core';
+import { Article } from './article.model';
+
+@Component({
+  selector: 'app-article',
+  templateUrl: './article.component.html',
+  styleUrls: ['./article.component.css']
+})
+export class ArticleComponent implements OnInit {
+  @HostBinding("attr.class") cssClass ="row";
+  
+  @Input() article!: Article;
+
+  constructor() { 
+    //this.article = new Article("Test", "http://test", 100);
+
+  }
+
+  voteUp(): boolean {
+    this.article.voteUp();
+    return false;
+  }
+
+  voteDown(): boolean {
+    this.article.voteDown();
+    return false;
+  }
+
+  ngOnInit(): void {
+  }
+
+}
